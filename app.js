b7d9fdcc80516739983947be3e495de32876f07c
Derek Stempien <watzup7856@gmail.com>
2025-11-08 23:31:54 -0500

    Derek Stempien

---
+++ app.js
@@ -0,0 +1,42 @@
+const menuButton1 = document.querySelector('#menu-button-1')
+const menuButton2 = document.querySelector('#menu-button-2')
+const menuButton3 = document.querySelector('#menu-button-3')
+const menuButton4 = document.querySelector('#menu-button-4')
+
+const menuPopup1 = document.querySelector('#menu-1')
+const menuPopup2 = document.querySelector('#menu-2')
+const menuPopup3 = document.querySelector('#menu-3')
+const menuPopup4 = document.querySelector('#menu-4')
+
+const menu1 = () => {
+    menuPopup1.style.display = "block"
+    menuPopup2.style.display = "none"
+    menuPopup3.style.display = "none"
+    menuPopup4.style.display = "none"
+}
+
+const menu2 = () => {
+    menuPopup1.style.display = "none"
+    menuPopup2.style.display = "block"
+    menuPopup3.style.display = "none"
+    menuPopup4.style.display = "none"
+}
+
+const menu3 = () => {
+    menuPopup1.style.display = "none"
+    menuPopup2.style.display = "none"
+    menuPopup3.style.display = "block"
+    menuPopup4.style.display = "none"
+}
+
+const menu4 = () => {
+    menuPopup1.style.display = "none"
+    menuPopup2.style.display = "none"
+    menuPopup3.style.display = "none"
+    menuPopup4.style.display = "block"
+}
+
+menuButton1.addEventListener('click', menu1)
+menuButton2.addEventListener('click', menu2)
+menuButton3.addEventListener('click', menu3)
+menuButton4.addEventListener('click', menu4)
